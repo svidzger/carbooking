333db2a3f5051543daebd67bfb64616d3fc7f68e
svidzger <german.svidzinskij@gmail.com>
2023-04-05 09:28:08 +0300

    first commit

---
+++ src/test/java/hh/backend/carbooking/CarbookingApplicationTests.java
@@ -0,0 +1,13 @@
+package hh.backend.carbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarbookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
